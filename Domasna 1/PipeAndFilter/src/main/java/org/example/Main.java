18c918a6f3d0bfd25058cba97208eb74362aac74
mtodo <milan.todorovik01@gmail.com>
2022-11-11 18:18:47 +0100

    Implements filling of empty variables

--- Domasna 1/PipeAndFilter/src/main/java/org/example/Main.java
+++ Domasna 1/PipeAndFilter/src/main/java/org/example/Main.java
@@ -1,10 +1,14 @@
 package org.example;
 
+
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
         Pipe<String> pipe = new Pipe<>();
         WithNameFilter withNameFilter = new WithNameFilter();
         RemoveEmptyFieldsFilter removeEmptyFieldsFilter = new RemoveEmptyFieldsFilter();
@@ -36,5 +40,63 @@ public class Main {
         scanner.close();
         filePrintWriter.flush();
         filePrintWriter.close();
+
+        SetEmptyFields setEmptyFields = new SetEmptyFields();
+        setEmptyFields.execute();
+
+    }
+
+    static class Building {
+        String name;
+        String city;
+        String address;
+        double lon;
+        double lat;
+        String amenity;
+        String website;
+        String phone;
+
+        public Building() {
+        }
+
+        public Building(String name, String address, String city, String website, String phone, String amenity, double lat, double lon) {
+            this.name = name;
+            this.city = city;
+            this.address = address;
+            this.lon = lon;
+            this.lat = lat;
+            this.amenity = amenity;
+            this.website = website;
+            this.phone = phone;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void setCity(String city) {
+            this.city = city;
+        }
+
+        public void setAddress(String address) {
+            this.address = address;
+        }
+
+        public void setLon(double lon) {
+            this.lon = lon;
+        }
+
+        public void setLat(double lat) {
+            this.lat = lat;
+        }
+
+        public void setAmenity(String amenity) {
+            this.amenity = amenity;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("%s,%s,%s,%s,%s,%s,%f,%f", name, address, city, amenity, phone, website, lat, lon);
+        }
     }
 }
---
+++ Domasna 1/PipeAndFilter/src/main/java/org/example/SetEmptyFields.java
@@ -0,0 +1,56 @@
+package org.example;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class SetEmptyFields {
+    public void execute() throws IOException {
+        Scanner scanner = new Scanner(new File("src/main/resources/result.csv"));
+        scanner.nextLine();
+
+        List<Main.Building> buildings = new ArrayList<>();
+
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] parts = line.split(",");
+            if (!parts[6].equals(""))
+                buildings.add(new Main.Building(parts[1], parts[2], parts[3], parts[4], parts[5], parts[6],
+                        Double.parseDouble(parts[9]), Double.parseDouble(parts[10])));
+            else if (!parts[7].equals("")) {
+                buildings.add(new Main.Building(parts[1], parts[2], parts[3], parts[4], parts[5], parts[7],
+                        Double.parseDouble(parts[9]), Double.parseDouble(parts[10])));
+            }
+        }
+
+        BufferedReader s = new BufferedReader(new FileReader(new File("src/main/resources/convert.csv")));
+        String line;
+        while ((line = s.readLine())!=null) {
+            String[] parts = line.split(",");
+            double lat = Double.parseDouble(parts[3]);
+            double lon = Double.parseDouble(parts[4]);
+            Main.Building building;
+            if(!parts[0].equals("")){
+                if (buildings.stream().filter(b -> b.lat == lat && b.lon == lon).collect(Collectors.toList()).size()>0) {
+                    building = buildings.stream().filter(b -> b.lat == lat && b.lon == lon).collect(Collectors.toList()).get(0);
+                    building.setName(parts[0]);
+                    building.setAddress(parts[1]);
+                    building.setCity(parts[2]);
+                }
+            }
+        }
+        scanner.close();
+        s.close();
+
+        File file = new File("src/main/resources/resultFinal.csv");
+        file.delete();
+
+        PrintWriter printWriter = new PrintWriter(new File("src/main/resources/resultFinal.csv"));
+        buildings.forEach(b -> printWriter.println(b));
+        printWriter.flush();
+        printWriter.close();
+
+    }
+}
