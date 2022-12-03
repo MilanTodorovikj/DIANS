b411ca4f507a97cf0c7f4d046a4ec7052a00e507
LinaNakova <lina.nakova@gmail.com>
2022-12-03 13:36:39 +0100

    new front end with new angular/node versions and map inside component

---
+++ Domasna 2/frontend/frontend/src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import * as Leaflet from 'leaflet';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'AngularOSM';
+
+  options: Leaflet.MapOptions = {
+    layers: getLayers(),
+    zoom: 8,
+    center: new Leaflet.LatLng(41.9981, 21.4254)
+  };
+}
+
+export const getLayers = (): Leaflet.Layer[] => {
+  return [
+    new Leaflet.TileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; OpenStreetMap contributors'
+    } as Leaflet.TileLayerOptions),
+    getMarkers()
+  ] as Leaflet.Layer[];
+};
+export const getMarkers = (): Leaflet.Marker[] => {
+  return [
+    new Leaflet.Marker(new Leaflet.LatLng(41.9981, 21.4254), {
+      icon: new Leaflet.Icon({
+        iconSize: [50, 41],
+        iconAnchor: [13, 41],
+        iconUrl: 'Domasna 2/frontend/frontend/node_modules/leaflet/src/images/marker.svg',
+      }),
+      title: 'Workspace'
+    } as Leaflet.MarkerOptions),
+  ] as Leaflet.Marker[];
+};
