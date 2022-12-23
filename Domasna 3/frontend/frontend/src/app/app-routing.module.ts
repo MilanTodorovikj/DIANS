ac59dd8c54b4a83d2ba8c25a951d578ac2c1900f
mtodo <milan.todorovik01@gmail.com>
2022-12-23 13:50:47 +0100

    adds login/logout functionality

--- Domasna 3/frontend/frontend/src/app/app-routing.module.ts
+++ Domasna 3/frontend/frontend/src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {AllSchoolsComponent} from "./all-schools/all-schools.component";
 import {SpecificSchoolComponent} from "./specific-school/specific-school.component";
+import {LoginComponent} from "./login/login.component";
+import {LogoutComponent} from "./logout/logout.component";
 
 const routes: Routes = [
   {path: 'all', component: AllSchoolsComponent},
   {path: 'detail/:id', component: SpecificSchoolComponent},
-  { path: '**', redirectTo: 'all', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'logout', component: LogoutComponent },
+  { path: '**', redirectTo: 'all', pathMatch: 'full'},
   {path: '', redirectTo: 'all', pathMatch: 'full'},
 ];
 
